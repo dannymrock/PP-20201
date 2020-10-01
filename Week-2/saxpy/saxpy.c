89b486a7e956a9ead09caf00e7b23a864a5f908b
Danny Munera <dannymunera@gmail.com>
2020-10-01 12:28:19 -0500

    Include saxpy example

---
+++ Week-2/saxpy/saxpy.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+void saxpy(int n,
+	   float a,
+	   float *x,
+	   float *restrict y)
+{
+  for (int i = 0; i < n; ++i)
+    y[i] = a * x[i] + y[i];
+}
+
+int main(int argc, char **argv)
+{
+  double ti, te;
+  int N = 1000000; 
+  
+  if (argc > 1)
+    N = atoi(argv[1]);
+  
+  float *x = (float*)malloc(N * sizeof(float));
+  float *y = (float*)malloc(N * sizeof(float));
+  
+  for (int i = 0; i < N; ++i) {
+    x[i] = 2.0f;
+    y[i] = 1.0f;
+  }
+  
+  ti = omp_get_wtime();
+  saxpy(N, 3.0f, x, y);
+  te = omp_get_wtime();
+
+  printf("Execution time %f \n", te - ti);
+
+  return 0;
+}
